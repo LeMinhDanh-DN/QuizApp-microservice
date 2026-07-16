20cd9ec94b2adf96396138e30da6c828a5c572a2
Minh Danh <minhdanhdn@gmail.com>
2026-07-16 21:26:11 +0700

    Init Question Microservice

---
+++ src/main/java/com/mr_n/questionservice/QuestionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mr_n.questionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionServiceApplication.class, args);
+    }
+
+}
