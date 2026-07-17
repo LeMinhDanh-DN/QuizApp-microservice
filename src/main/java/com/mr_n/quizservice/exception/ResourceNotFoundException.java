4c4c153882b33dc6ef63c52cf24ef2b64e0796dc
Minh Danh <minhdanhdn@gmail.com>
2026-07-17 11:46:59 +0700

    feat(quiz): implement exception handler & custom exceptions

---
+++ src/main/java/com/mr_n/quizservice/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.mr_n.quizservice.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
