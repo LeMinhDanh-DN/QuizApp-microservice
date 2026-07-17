1c55a4f68260c16037424780018260c33f45113f
Minh Danh <minhdanhdn@gmail.com>
2026-07-17 11:39:03 +0700

    feat(quiz): initialize entities

---
+++ src/main/java/com/mr_n/quizservice/model/Quiz.java
@@ -0,0 +1,22 @@
+package com.mr_n.quizservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String category;
+
+    private List<Integer> questionIds;
+}
