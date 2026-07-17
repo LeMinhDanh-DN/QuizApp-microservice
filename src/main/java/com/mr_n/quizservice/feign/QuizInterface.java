9df47ac35a4f200ad65bbdd617de08dfda026f9d
Minh Danh <minhdanhdn@gmail.com>
2026-07-17 19:07:43 +0700

    feat(quiz): implement QuizInterface to call question service.

---
+++ src/main/java/com/mr_n/quizservice/feign/QuizInterface.java
@@ -0,0 +1,26 @@
+package com.mr_n.quizservice.feign;
+
+import com.mr_n.quizservice.model.QuestionWrapper;
+import com.mr_n.quizservice.model.Response;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(name = "QUESTION-SERVICE")
+public interface QuizInterface {
+
+    @GetMapping("/generate")
+    public ResponseEntity<List<Integer>> getQuestionForQuiz (@RequestParam String category,
+                                                             @RequestParam Integer numQ);
+
+    @PostMapping("getQuestions")
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsByIds(@RequestBody List<Integer> questionIds);
+
+    @PostMapping("getScore")
+    public ResponseEntity<Integer> getScore(@RequestBody List<Response> responses);
+}
