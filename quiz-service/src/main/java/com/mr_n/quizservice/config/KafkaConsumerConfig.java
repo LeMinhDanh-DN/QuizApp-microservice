769fca32975a67ab43634c84dcf85334f8893fe3
Minh Danh <minhdanhdn@gmail.com>
2026-08-21 22:46:32 +0700

    fix(kafka): configure ErrorHandlingDeserializer & JsonDeserializer

--- quiz-service/src/main/java/com/mr_n/quizservice/config/KafkaConsumerConfig.java
+++ quiz-service/src/main/java/com/mr_n/quizservice/config/KafkaConsumerConfig.java
@@ -1,5 +1,6 @@
 package com.mr_n.quizservice.config;
 
+import com.mr_n.quizservice.model.event.QuizResultEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -25,11 +27,16 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "quiz-group");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        props.put("spring.json.trusted.packages", "*");
-        props.put("spring.json.type.mapping",
-                "com.mr_n.questionservice.event.QuizResultEvent:com.mr_n.quizservice.event.QuizResultEvent");
+
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
+
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, QuizResultEvent.class.getName());
 
         return new DefaultKafkaConsumerFactory<>(props);
     }
